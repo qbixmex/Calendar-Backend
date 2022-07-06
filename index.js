36cc438b1bbd91a254fde1ca07cf6c25b9c75590
Daniel González Briseño <daniel.gb@globant.com>
2022-07-05 22:21:37 -0500

    02 - Configuring Express

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.clear();
-console.log('Calendar Backend');
+const express = require('express');
+
+// Create Server
+const app = express();
+
+app.get('/', (_reqquest, response) => {
+  response.status(200).json({ ok: true });
+});
+
+// Listen App
+app.listen(4000, () => {
+  console.log(`Server running on port: ${ 4000 }`);
+});
