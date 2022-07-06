c0f2e78ef4937512365df98c7461ecbafd340d9e
Daniel González Briseño <daniel.gb@globant.com>
2022-07-06 15:41:53 -0500

    22 - Update Event

--- controllers/events.js
+++ controllers/events.js
@@ -41,10 +41,51 @@ const createEvent = async ( req = request, res = response ) => {
 };
 
 const updateEvent = async ( req = request, res = response ) => {
-  res.json({
-    ok: true,
-    msg: 'Event Updated'
-  });
+
+  const eventId = req.params.id;
+  const uid = req.uid;
+
+  try {
+
+    const event = await Event.findById(eventId);
+
+    if ( !event ) {
+      return res.status(404).json({
+        ok: false,
+        msg: `El evento no existe por el ID: ${ eventId }`
+      });
+    }
+
+    if ( event.user.toString() !== uid ) {
+      return res.status(401).json({
+        ok: false,
+        msg: `No tienes suficientes privilegios para realizar esta acción`
+      });
+    }
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    };
+
+    const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+    res.json({
+      ok: true,
+      event: updatedEvent
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      ok: false,
+      msg: 'Error de servidor, hable con el administrador'
+    });
+
+  }
+
 };
 
 const deleteEvent = async ( req = request, res = response ) => {
