09f11eeaaee8c12321d914f1e249a2ca907e853c
Daniel González Briseño <daniel.gb@globant.com>
2022-07-06 02:21:17 -0500

    10 - Create User in Database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,24 @@
 const { response } = require("express");
+const User = require("../models/User");
 
-const userCreate = (req, res = response) => {
+const userCreate = async (req, res = response) => {
 
   const { name, email, password } = req.body;
 
   // TODO: HashPassword
   // TODO: save to mongo db
-  
-  res.status(201).json({
-    ok: true,
-    user: { name, email, password },
-  });
+
+  try {
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).json({ ok: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Error de servidor, hable con el administrador'
+    });
+  }
 
 };
 
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [ true, "Name is required" ],
+  },
+  email: {
+    type: String,
+    required: [ true, "Email is required" ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [ true, "Password is required" ],
+  },
+}, {
+  timestamps: true,
+  versionKey: false,
+});
+
+module.exports = model( 'User', UserSchema );
