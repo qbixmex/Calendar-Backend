d77ba96d01f7ba6a247c66de6e0d64c840bee5a8
Daniel González Briseño <daniel.gb@globant.com>
2022-07-05 22:32:01 -0500

    03 - Root and Public Route, DotEnv

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 
 // Create Server
 const app = express();
 
+// Public Directory
+app.use( express.static('public') );
+
+// Routes
 app.get('/', (_reqquest, response) => {
   response.status(200).json({ ok: true });
 });
 
 // Listen App
-app.listen(4000, () => {
-  console.log(`Server running on port: ${ 4000 }`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port: ${ process.env.PORT }`);
 });
