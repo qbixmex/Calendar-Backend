14e1de269ae43ffb140089d7103e0797a49e48ce
Daniel González Briseño <daniel.gb@globant.com>
2022-07-07 15:22:53 -0500

    25 - Response to public/index.html with wild card

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ app.use( express.static('public') );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
+app.use('*', (_, response) => {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
 // Listen App
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port: ${ process.env.PORT }`);
